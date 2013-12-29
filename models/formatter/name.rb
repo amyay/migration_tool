b844ee350ee0c0e05683672e58e4cb3aa373935d
amy <amy@zendesk.com>
2013-12-30 02:11:38 +0800

    leave spaces alone unless it's before and after

--- models/formatter/name.rb
+++ models/formatter/name.rb
@@ -1,6 +1,6 @@
 class Formatter::Name < Formatter
   def formatted
     return @raw_string if @raw_string.nil?
-    @raw_string.gsub /[\s\t\r\n]/, ''
+    @raw_string.strip.gsub /[\t\r\n]/, ''
   end
 end
