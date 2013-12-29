25b3f4d018cef0d690ff1dcf66c36c3b77b8c4e1
amy <amy@zendesk.com>
2013-12-29 23:45:52 +0800

    each importer needs to define #import_raw

--- models/importer.rb
+++ models/importer.rb
@@ -2,5 +2,17 @@ class Importer
   def initialize csv
     @csv = csv
     @logger = Logger.new STDOUT
+    @type = ''
+  end
+
+  def import
+    @logger.debug "Importing #{@type}s from #{@csv}"
+    count = 0
+    CSV.foreach(@csv, :headers=>true) do |row|
+      import_row row
+      count += 1
+      @logger.debug "Processed #{count} #{@type}s." if count % 500 == 0
+    end
+    @logger.info "Processed #{count} #{@type}s from #{@csv}"
   end
 end
--- models/importer/user.rb
+++ models/importer/user.rb
@@ -1,24 +1,21 @@
 require 'csv'
 
 class Importer::User < Importer
-  def import
-    @logger.debug "Importing users from #{@csv}"
-    count = 0
-    CSV.foreach(@csv, :headers=>true) do |row|
-      email = Formatter::Email.new row['Email Address']
-      name = Formatter::Name.new row['Full Name']
-      phone = Formatter::Phone.new row['Phone']
+  def initialize csv
+    super
+    @type = 'user'
+  end
 
-      user_class = role_to_user_class row['Role']
-      user = user_class.find_or_create_by_email email.to_s
-      user.name = name.to_s
-      user.phone = phone.to_s
-      user.save!
+  def import_row row
+    email = Formatter::Email.new row['Email Address']
+    name = Formatter::Name.new row['Full Name']
+    phone = Formatter::Phone.new row['Phone']
 
-      count += 1
-      @logger.debug "Processed #{count} users." if count % 500 == 0
-    end
-    @logger.info "Processed #{count} users from #{@csv}"
+    user_class = role_to_user_class row['Role']
+    user = user_class.find_or_create_by_email email.to_s
+    user.name = name.to_s
+    user.phone = phone.to_s
+    user.save!
   end
 
 private
