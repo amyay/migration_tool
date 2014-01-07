ed5ae9eea4531ca2019f2aae9991c9ce10ac82a6
amy <amy@zendesk.com>
2014-01-07 19:50:40 +0800

    Revert "- added @attribute_to_column_name hash table in initialize" reverting @attribute_to_column_name to avoid getting more confuse
    This reverts commit 8af9ef373cac531442d580caedd49eec808d1990.

--- models/importer/user.rb
+++ models/importer/user.rb
@@ -4,31 +4,19 @@ class Importer::User < Importer
   def initialize csv
     super csv
     @type = 'user'
-    @attribute_to_column_name = {
-      email: "Email Address",
-      name: "Full Name",
-      phone: "Phone",
-      role: "Role"
-    }
-
-  end
-
-  def column_name attribute
-    @attribute_to_column_name attribute
   end
 
   def import_row row
-    email = Formatter::Email.new column_name(:email)
-    name = Formatter::Name.new column_name(:name)
-    phone = Formatter::Phone.new column_name(:phone)
+    email = Formatter::Email.new row['Email Address']
+    name = Formatter::Name.new row['Full Name']
+    phone = Formatter::Phone.new row['Phone']
 
-    user_class = role_to_user_class column_name(:role)
+    user_class = role_to_user_class row['Role']
     user = user_class.find_or_create_by_email email.formatted
     user.name = name.formatted
     user.phone = phone.formatted
     user.save!
   end
-
 
 private
   def role_to_user_class role
