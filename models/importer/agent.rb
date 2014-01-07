8af9ef373cac531442d580caedd49eec808d1990
amy <amy@zendesk.com>
2014-01-07 19:05:26 +0800

    - added @attribute_to_column_name hash table in initialize - update code to use function "column_name" instead of hardcoding name of header row within code

--- models/importer/agent.rb
+++ models/importer/agent.rb
@@ -4,17 +4,27 @@ class Importer::Agent < Importer
   def initialize csv
     super csv
     @type = 'agent'
+    @attribute_to_column_name = {
+      email: "Email",
+      name: "Name",
+      phone: "Phone",
+      group: "Group"
+    }
+  end
+
+  def column_name attribute
+    @attribute_to_column_name attribute
   end
 
   def import_row row
-    email = Formatter::Email.new row['Email']
-    name = Formatter::Name.new row['Name']
-    phone = Formatter::Phone.new row['Phone']
+    email = Formatter::Email.new column_name(:email)
+    name = Formatter::Name.new column_name(:name)
+    phone = Formatter::Phone.new column_name(:phone)
     user = ::User::Agent.find_or_create_by_email email.formatted
     user.name = name.formatted
     user.phone = phone.formatted
 
-    group = ::Group.find_or_create_by_name row['Group']
+    group = ::Group.find_or_create_by_name column_name(:group)
     user.groups << group
     user.save!
   end
