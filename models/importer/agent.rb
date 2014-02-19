94aa1b468639e43b939afaf35a914ce93f48b9bb
amy <amy@zendesk.com>
2014-02-19 22:13:40 +0800

    added similar email checks (as previous commit) for agent importer

--- models/importer/agent.rb
+++ models/importer/agent.rb
@@ -10,6 +10,13 @@ class Importer::Agent < Importer
     email = Formatter::Email.new row['Email']
     name = Formatter::Name.new row['Name']
     phone = Formatter::Phone.new row['Phone']
+
+    email.validate! do
+      address = name.formatted.gsub " ", "."
+      domain = "legacymedidata.com"
+      made_up_address = "#{address}@#{domain}"
+    end
+
     user = ::User::Agent.find_or_create_by_email email.formatted
     user.name = name.formatted
     user.phone = phone.formatted
