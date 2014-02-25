d579351b04ee04eff3051f3b27de2c4d3847f20a
amy <amy@zendesk.com>
2014-02-25 13:00:14 +0800

    user validation

--- models/user.rb
+++ models/user.rb
@@ -3,8 +3,40 @@ class User < ActiveRecord::Base
   has_many :requested_tickets, class_name: "Ticket", foreign_key: "requester_id"
   has_many :comments, foreign_key: 'author_id'
 
+  # user must have a name
+  validates :name, presence: true
+
+  # user must have valid email address
+  validates :email, uniqueness: true
+  validate :validate_email
+
+  before_validation :ensure_email_is_valid, on: :create
+
   def name= name
     super
     self.shortname = (Formatter::Shortname.new self.name).formatted
   end
+
+  protected
+    def validate_email
+      if !valid_email?
+        errors.add(:email, "invalid email address")
+      end
+    end
+
+    def ensure_email_is_valid
+      if !valid_email?
+        if self.email.include? '@'
+          # hack away and adjust email as needed
+          # for medidata, its' the "=" in the emails
+          temp_email = self.email.split '@'
+          temp_email[0].gsub! '=', '-'
+          self.email = temp_email[0] + '@'+ temp_email[1]
+        end
+      end
+    end
+
+    def valid_email?
+      return !(self.email =~ /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i).nil?
+    end
 end
