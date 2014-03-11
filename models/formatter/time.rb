220fbd2297bda4f5e93b0b66a3b0037122756a59
amy <amy@zendesk.com>
2014-03-11 23:55:05 +0800

    adjust formatter::time for tripadvisor's time stamps

--- models/formatter/time.rb
+++ models/formatter/time.rb
@@ -6,10 +6,10 @@ class Formatter::Time < Formatter
     # Time.parse(self).strftime("%m/%d/%Y %T")
     # Time.parse(self).strftime("%Y-%m-%d %T GMT-08:00")
     # for muscogee
-    # Time.strptime(self,"%m/%d/%Y %H:%M:%S %p").strftime("%Y-%m-%d %T GMT-05:00")
-    # Time.strptime(self,"%m/%d/%y %H:%M").strftime("%Y-%m-%d %T GMT-05:00")
-    # Time.strptime(self,"%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %T GMT-05:00")
-    # Time.strptime(@raw_string,"%m/%d/%y %H:%M").strftime("%Y-%m-%d %T GMT-05:00")
-    ::Time.strptime(@raw_string,"%m/%d/%y").strftime("%Y-%m-%d")
+    # ::Time.strptime(@raw_string,"%m/%d/%Y %H:%M:%S %p").strftime("%Y-%m-%d %T GMT-05:00")
+    # ::Time.strptime(@raw_string,"%m/%d/%y %H:%M").strftime("%Y-%m-%d %T GMT-05:00")
+    ::Time.strptime(@raw_string,"%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d %T GMT-05:00")
+    # ::Time.strptime(@raw_string,"%m/%d/%y %H:%M").strftime("%Y-%m-%d %T GMT-05:00")
+    # ::Time.strptime(@raw_string,"%m/%d/%y").strftime("%Y-%m-%d")
   end
 end
