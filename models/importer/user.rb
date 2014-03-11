4fec658d2f7364d8052ece61afcea3d1910d09dd
amy <amy@zendesk.com>
2014-03-11 23:51:24 +0800

    adjust user importer to read all as end users, and adjust validation domain for tripadvisor

--- models/importer/user.rb
+++ models/importer/user.rb
@@ -7,19 +7,19 @@ class Importer::User < Importer
   end
 
   def import_row row
-    email = Formatter::Email.new row['Email Address']
+    email = Formatter::Email.new row['Email']
     name = Formatter::Name.new row['Full Name']
-    phone = Formatter::Phone.new row['Phone']
+
     email.validate! do
       address = name.formatted.gsub " ", "."
-      domain = "legacymedidata.com"
+      domain = "legacyuser-tripadvisor.com"
       made_up_address = "#{address}@#{domain}"
     end
 
-    user_class = role_to_user_class row['Role']
-    user = user_class.find_or_create_by_email email.formatted
+    # user_class = role_to_user_class row['Role']
+    # user = user_class.find_or_create_by_email email.formatted
+    user = ::User.find_or_create_by_email email.formatted
     user.name = name.formatted
-    user.phone = phone.formatted
     user.save!
   end
 
