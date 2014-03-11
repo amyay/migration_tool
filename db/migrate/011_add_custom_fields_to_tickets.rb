2b124ea4f8227afa4556aabe77a5a73aeed13912
amy <amy@zendesk.com>
2014-03-11 23:48:26 +0800

    add specific custom fields for tripadvisor

--- db/migrate/011_add_custom_fields_to_tickets.rb
+++ db/migrate/011_add_custom_fields_to_tickets.rb
@@ -1,7 +1,12 @@
 class AddCustomFieldsToTickets < ActiveRecord::Migration
   def change
-    add_column :tickets, :legacy_id, :string
-    add_column :tickets, :sponsor_study, :string
-    add_column :tickets, :resolution, :string
+    add_column :tickets, :legacy_id,   :string
+    add_column :tickets, :queue,       :string
+    add_column :tickets, :emailed_to,  :string
+    add_column :tickets, :userIP,      :string
+    add_column :tickets, :WEbagent,    :string
+    add_column :tickets, :ReferrerURL, :string
+    add_column :tickets, :Webcookie,   :string
+    add_column :tickets, :ThreadId,    :string
   end
 end
